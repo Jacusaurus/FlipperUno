2e6c5d250bd26bb9d7b467b518b3b33671fc85f9
Kevin Smith <kasmith0193@gmail.com>
2022-04-15 19:41:11 -0400

    Finisshed CardGenorator Class

--- Assets/Interfacing Classes/CardGenerator.cs
+++ Assets/Interfacing Classes/CardGenerator.cs
@@ -24,18 +24,18 @@ public class CardGenerator : MonoBehaviour
 
 	}
 
-	public int NumberCard;
-	public int DrawTwo;
-	public int Reverse;
-	public int Skip;
+	public int Number = 700;
+	public int DrawTwo = 75;
+	public int Reverse = 75;
+	public int Skip = 75;
 
-	public int Wild;
-	public int WildDrawFour; 
+	public int Wild = 37;
+	public int WildDrawFour 37; 
 
-	public int Red;
-	public int Blue;
-	public int Green;
-	public int Yellow;
+	public int Red = 25;
+	public int Blue = 25;
+	public int Green = 25;
+	public int Yellow = 25;
 
 
 	public enum Color
@@ -47,33 +47,96 @@ public class CardGenerator : MonoBehaviour
 		YELLOW
 	}
 
-	
-
+	public enum Type
+	{
+		NUMBER,
+		DRAWTWO,
+		REVERSE,
+		SKIP,
+		WILD,
+		WILDDRAWFOUR
+	}
 
     public BaseCard GetNewCard()
 	{
-		int type = Random.Range(1, NumberCard + DrawTwo + Reverse + Skip + Wild + WildDrawFour);
-		int color = Random.Range(1, Red + Blue + Green + Yellow);
+		int typerand = Random.Range(1, Number + DrawTwo + Reverse + Skip + Wild + WildDrawFour + 1);
+		int colorrand = Random.Range(1, Red + Blue + Green + Yellow + 1);
 		int number = Random.Range(0, 10);
 
-		BaseCard NewCard = new BaseCard();
+		Type type = GetType(typerand);
+		Color color = GetColor(colorrand);
 
-
-
-
-
+		BaseCard NewCard =  BaseCard.CreateCard(color, number, type);
 		return NewCard;
 	}
 
-	int string GetType()
+	private Type GetType(int rand)
 	{
+		Type type = Type.NUMBER;
 
+		Range numberRange = new Range();
+		Range drawTwoRange = new Range();
+		Range reverseRange = new Range();
+		Range skipRange = new Range();
+		Range wildRange = new Range();
+		Range wildDrawFourRange = new Range();
+
+		int i = 0;
+		numberRange.lower = 1;
+		i += Number;
+		numberRange.upper = i;
+
+		drawTwoRange.lower = i + 1;
+		i += DrawTwo;
+		drawTwoRange.upper = i;
+
+		reverseRange.lower = i + 1;
+		i += Reverse;
+		reverseRange.upper = i;
+
+		skipRange.lower = i + 1;
+		i += Skip;
+		skipRange.upper = i;
+
+		wildRange.lower = i + 1;
+		i += Wild;
+		wildRange.upper = i;
+
+		wildDrawFourRange.lower = i + 1;
+		i += WildDrawFour;
+		wildDrawFourRange.upper = i;
+
+		if (rand >= numberRange.lower && rand <= numberRange.upper)
+		{
+			type = Type.NUMBER;
+		}
+		if (rand >= drawTwoRange.lower && rand <= drawTwoRange.upper)
+		{
+			type = Type.DRAWTWO;
+		}
+		if (rand >= reverseRange.lower && rand <= reverseRange.upper)
+		{
+			type = Type.REVERSE;
+		}
+		if (rand >= skipRange.lower && rand <= skipRange.upper)
+		{
+			type = Type.SKIP;
+		}
+		if (rand >= wildRange.lower && rand <= wildRange.upper)
+		{
+			type = Type.WILD;
+		}
+		if (rand >= wildDrawFourRange.lower && rand <= wildDrawFourRange.upper)
+		{
+			type = Type.WILDDRAWFOUR;
+		}
+		return type;
 	}
 
 
 	private Color GetColor(int rand)
 	{
-		Color color;
+		Color color = Color.RED;
 
 		Range redRange = new Range();
 		Range blueRange = new Range();
